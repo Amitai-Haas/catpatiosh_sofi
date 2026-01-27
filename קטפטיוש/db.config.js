3bbf86016ee2f49efeb5fdfdf785af5cc907dc33
Amitai-Haas <haasam@post.bgu.ac.il>
2026-01-27 21:24:15 +0200

    Create db.config.js

---
+++ קטפטיוש/db.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+    HOST: 'localhost',
+    PORT: 8080,              //הייתה לי בעיה בבסיס הנתונים אז הגדרתי מחדש את הפורט
+    USER: 'admin',           // המשתמש 
+    PASSWORD: '1234',        // הסיסמה 
+    DB: 'project'            //בסיס הנתונים
+};
